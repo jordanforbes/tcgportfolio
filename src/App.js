38ae6aef8b3f55b356be4431a9d149846015e486
jordanforbes <jordforb@gmail.com>
2021-02-15 06:30:33 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import bernie from './Assets/Bernie.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={bernie} className="card" alt="bernie" /> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
